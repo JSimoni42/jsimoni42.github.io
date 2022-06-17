85ee17285029f400241f863763fafd970bd54bce
John Simoni <john.simoni@hiya.com>
2022-06-17 09:09:40 -0600

    Exclude konva from building SSR

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,6 +23,14 @@ module.exports = {
         icon: `src/images/thumbs-up.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-plugin-browser-dependencies',
+      options: {
+        dependencies: [
+          'konva'
+        ]
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
